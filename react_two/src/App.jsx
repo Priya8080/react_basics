52bfa9f6ef6f442b1dd4c696e89f6feafe4d944c
Priya Kumari <kumaripriya8084785476@gmail.com>
2025-06-30 15:50:38 +0530

    added card

--- react_two/src/App.jsx
+++ react_two/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import UserCard from "./components/UserCard"; 
+import star from "./assets/star.jpg"
+import girl from "./assets/girl.jpg"
+import gurl from "./assets/girl2.jpg"
 
 function App() {
   return (
     <div className='container'>
-      <UserCard name="Priya Singh" desc="Overthinker"/>
-      <UserCard name="Sakshi Singh" desc="Mental"/>
-      <UserCard name="Archana Raj" desc="Psychopath"/>
+      <UserCard name="Priya Singh" desc="Overthinker" image={star}/>
+      <UserCard name="Sakshi Singh" desc="Mental" image={girl}/>
+      <UserCard name="Archana Raj" desc="Psychopath" image={gurl}/>
     </div>
   );
 }
--- react_two/src/components/UserCard.jsx
+++ react_two/src/components/UserCard.jsx
@@ -6,7 +6,7 @@ const UserCard = (props) => {
   return (
     <div className='user-container'>
       <p id='user-name'>{props.name}</p>
-      <img id='user-img' src={star} alt="Priya" />
+      <img id='user-img' src={props.image} alt={props.name} />
       <p id='user-dec'>{props.desc}</p>
     </div>
   )
