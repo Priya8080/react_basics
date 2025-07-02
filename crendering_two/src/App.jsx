3aff0d7806a0aa0e8e54b1e309e8549907d9cf1d
Priya Kumari <kumaripriya8084785476@gmail.com>
2025-07-02 14:44:08 +0530

    ternary

---
+++ crendering_two/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import LoginBtn from './components/LoginBtn';
+import LogoutBtn from './components/LogoutBtn';
+
+function App() {
+  const [isLoggedIn, setLoggedIn] = useState(true);
+  if(!isLoggedIn){
+    return(
+      <LoginBtn/>
+    )
+  }
+     return(
+      <div>
+         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, praesentium.</h1>
+         <div>
+          {isLoggedIn && <LogoutBtn/>}
+         </div>
+      </div>
+     )
+
+  // return (
+  //  <div>
+  //   {isLoggedIn ? <LogoutBtn/> : <LoginBtn/>}
+  //  </div>
+  // )
+}
+
+export default App
