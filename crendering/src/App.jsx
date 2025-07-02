09724993d174c871c4d4d738d94593216fd4c63a
Priya Kumari <kumaripriya8084785476@gmail.com>
2025-07-02 14:23:15 +0530

    if-else

---
+++ crendering/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import LogoutBtn from './components/LogoutBtn';
+import LoginBtn from './components/LoginBtn';
+
+function App() {
+  const[isLoggedIn, setLoggedIn] = useState(false);
+  if(isLoggedIn){
+       return(
+        <LogoutBtn/>
+       )
+  }
+  else{
+      return(
+        <LoginBtn/>
+      )
+  }
+}
+
+export default App
