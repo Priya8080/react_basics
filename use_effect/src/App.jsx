29307a98b0b01d8fa04a22a1ce1c6fe1fc3690bf
Priya Kumari <kumaripriya8084785476@gmail.com>
2025-07-02 16:59:24 +0530

    added

---
+++ use_effect/src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect } from 'react'
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(1);
+//first -> side-effect function
+//second -> clean-up function
+//third -> comma separated dependency list
+
+//variation :1 -> runs on every render
+  //useEffect(() => {
+  //alert("I will run on each render")
+  //})
+
+//variation: 2 -> that runs only first render
+//useEffect(() => {
+  //alert("I will run on only first render")
+//},[])
+
+//variation: 3 
+//useEffect(() => {
+  //alert("I will run every time when count is updated")
+//},[count])
+
+//variation: 4 -> multiple dependencies
+useEffect(() => {
+   alert("I will run every time when count/total is updated")
+},[count,total])
+  function handleClick(){
+    setCount(count+1);
+  }
+
+   function handleClick(){
+    setTotal(total+1);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>
+        Update Count
+      </button>
+      <br/>
+      Count is: {count}
+
+       <button onClick={handleClick}>
+        Update Total
+      </button>
+      <br/>
+      Total is: {total}
+    </div>
+  )
+}
+
+export default App
