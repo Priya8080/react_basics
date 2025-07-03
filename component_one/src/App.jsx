41f70ef21204c5b37563a6cd6b1319dd29dbbdd4
Priya Kumari <kumaripriya8084785476@gmail.com>
2025-07-03 15:55:09 +0530

    created a component

---
+++ component_one/src/App.jsx
@@ -0,0 +1,12 @@
+import UserCard from "./components/UserCard";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <UserCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ component_one/src/components/UserCard.jsx
@@ -0,0 +1,10 @@
+function UserCard(){
+    return(
+        <div className="card">
+           <h2>Priya Singh</h2>
+           <p>Frontend Developer</p>
+        </div>
+    );
+}
+
+export default UserCard;
