339b05b0b38c1d51639116c485631db1a7daaf85
Priya Kumari <kumaripriya8084785476@gmail.com>
2025-07-04 16:09:23 +0530

    quote

---
+++ use_effect_one/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import quotes from './quotes.json';  // ✅ local import
+
+function App() {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const selectedQuote = quotes[randomIndex].text;
+    setQuote(selectedQuote);
+  }, []);
+
+  return (
+    <div>
+      <h1>Offline Quote Machine</h1>
+      <p>{quote ? quote : "Loading..."}</p>
+    </div>
+  );
+}
+
+export default App;
+
